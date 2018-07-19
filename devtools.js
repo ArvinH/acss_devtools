5c3c79b4d916eb2ad2ce93842dfc3ba23a101f36
Weep Shih <ycshih@yahoo-inc.com>
2018-07-19 14:40:45 +0800

    remove communicating

--- devtools.js
+++ devtools.js
@@ -8,15 +8,44 @@
     (sidebar) => {
       sidebar.setPage("Sidebar.html");
 
-      const getAcssClass = () => {
+      const page_generateAtomicClass = (selectedElementCssText = '') => {
+        const inlineStyleText = selectedElementCssText;
+        const inlineStyleArray = inlineStyleText && inlineStyleText.split('; ') || [];
+        const styleMap = [];
+        inlineStyleArray.forEach((styleString) => {
+          if (!styleString) {
+            return;
+          }
+          const styleArray = styleString.replace(';', '').split(': ');
+          const style = {
+            [styleArray[0]]: styleArray[1]
+          };
+
+          let acssClass = acssRuleMap[`${styleArray[0]}: ${styleArray[1]}`];
+          if (acssClass) {
+            styleMap.push(acssClass);
+          } else {
+            acssClass = acssRuleMap[`${styleArray[0]}: value`]
+            styleMap.push(acssClass.replace('<value> or ', '').replace('<custom-param>', styleArray[1]));
+          }
+        });
+
+        return {
+          acss: styleMap.join(' ').toString()
+        };
+      }
+
+      const getAcssClass = (extPanelWindow) => {
         chrome.devtools.inspectedWindow.eval(
-          "setSelectedElement($0)",
-          { useContentScriptContext: true }
+          "window.$0.style.cssText",
+          function (result, isException) {
+            var status = extPanelWindow.document.querySelector("#app");
+            const acssClass = page_generateAtomicClass(result);
+            status.innerHTML = acssClass.acss;
+          }
         );
       };
 
-      getAcssClass()
-      chrome.devtools.panels.elements.onSelectionChanged.addListener(
-        getAcssClass);
+      sidebar.onShown.addListener(getAcssClass);
     });
 })();
--- sidebar.js
+++ sidebar.js
@@ -1,46 +1,4 @@
 (() => {
-    const page_generateAtomicClass = (selectedElementCssText) => {
-        const inlineStyleText = selectedElementCssText;
-        const inlineStyleArray = inlineStyleText && inlineStyleText.split('; ') || [];
-        const styleMap = [];
-        inlineStyleArray.forEach((styleString) => {
-            if (!styleString) {
-                return;
-            }
-            const styleArray = styleString.replace(';', '').split(': ');
-            const style = {
-                [styleArray[0]]: styleArray[1]
-            };
-
-            let acssClass = acssRuleMap[`${styleArray[0]}: ${styleArray[1]}`];
-            if (acssClass) {
-                styleMap.push(acssClass);
-            } else {
-                acssClass = acssRuleMap[`${styleArray[0]}: value`]
-                styleMap.push(acssClass.replace('<value> or ', '').replace('<custom-param>', styleArray[1]));
-            }
-        });
-
-        return {
-            acss: styleMap.join(' ').toString()
-        };
-    }
-
-
-    const backgroundPageConnection = chrome.runtime.connect({
-        name: "panel"
-    });
-
-    backgroundPageConnection.postMessage({
-        name: 'init',
-        tabId: chrome.devtools.inspectedWindow.tabId
-    });
-    backgroundPageConnection.onMessage.addListener((message) => {
-        // Handle responses from the background page, if any
-        const acssClass = page_generateAtomicClass(message.cssText);
-        document.querySelector('#app').innerHTML = acssClass.acss;
-    });
-
     const copyToClipboard = str => {
         const el = document.createElement('textarea');
         el.value = str;
@@ -58,5 +16,4 @@
         const text = document.querySelector('#app').innerHTML;
         copyToClipboard(text)
     });
-
 })();
